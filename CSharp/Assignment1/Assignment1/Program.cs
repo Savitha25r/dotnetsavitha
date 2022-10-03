1c355852d6ebbb9f5ee6d9eaf1900b76b64a8efb
Savitha25r <114404662+Savitha25r@users.noreply.github.com>
2022-10-03 07:57:28 -0700

    Add files via upload
    Assignment1,2,3

---
+++ CSharp/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*************** Program 1*************");
+
+            Console.WriteLine("Enter the number to check both values are equal");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Program program = new Program();
+            program.result(num1, num2);
+            Console.WriteLine("");
+
+            Console.WriteLine("***************Program2 *************");
+            Console.WriteLine("enter a number to check positive or negative");
+            Example2 ex = new Example2();
+            ex.PositiveOrNegative();
+            Console.WriteLine("");
+
+            Console.WriteLine("*********Program 3************");
+            Example3 ex3 = new Example3();
+            ex3.oper();
+
+            Console.WriteLine("*********Program 4************");
+            Example4 ex4 = new Example4();
+            ex4.cal();
+
+
+            Console.WriteLine("*********Program 5************");
+            Example5 ex5 = new Example5();
+            ex5.cel();
+
+            Console.ReadKey();
+        }
+        public void result(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine("{0} and {1} are equal ", num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are not equal ", num1, num2);
+            }
+        }
+
+
+
+    }
+    public class Example2
+    {
+        public int val1 = 0;
+        public int val2 = Convert.ToInt32(Console.ReadLine());
+        public void PositiveOrNegative()
+        {
+            if (val1 > val2)
+            {
+                Console.WriteLine($"the given number {val2} is Negative");
+            }
+            else
+            {
+                Console.WriteLine($"the given number {val2} is Positive");
+            }
+        }
+    }
+
+
+    //program3
+    public class Example3
+    {
+
+        public void oper()
+        {
+            int x, y;
+            char operation;
+
+            Console.Write("Input first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input operation: ");
+
+            operation = Convert.ToChar(Console.ReadLine());
+            Console.Write("Input second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+            if (operation == '+')
+                Console.WriteLine("Excepted output: {0} + {1} = {2}", x, y, x + y);
+
+
+            else if (operation == '-')
+                Console.WriteLine("Excepted output: {0} - {1} = {2}", x, y, x - y);
+
+            else if ((operation == 'x') || (operation == '*'))
+                Console.WriteLine("Excepted output: {0} * {1} = {2}", x, y, x * y);
+
+            else if (operation == '/')
+                Console.WriteLine("Excepted output: {0} / {1} = {2}", x, y, x / y);
+
+            else
+                Console.WriteLine("Wrong Character");
+
+        }
+    }
+
+    }
+
+//program4
+public class Example4
+{
+
+    public void cal()
+    {
+
+        int csharp, html, sql, total;
+        {
+            Console.WriteLine("enter the csharp marks");
+            csharp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the html marks");
+            html = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the sql marks");
+            sql = Convert.ToInt32(Console.ReadLine());
+            total = csharp + html + sql;
+            Console.WriteLine("The total marks is:{0}", total);
+            {
+                if (total < 49)
+                    Console.WriteLine("The student is Fail");
+
+
+
+                else if (total > 50)
+                    Console.WriteLine("The student is pass");
+
+
+
+            }
+        }
+            }
+
+        }
+
+
+
+            
+
+
+
+//program5
+
+
+public class Example5
+{
+
+    public void cel()
+    {
+        double celsius;
+        double fahrenheit = 97;
+        Console.WriteLine("Fahrenheit: " + fahrenheit);
+        celsius = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine("Celsius: " + celsius);
+        
+    }
+}
---
+++ CSharp/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+
+    class Program
+    {
+        public int AccountNum;
+        public string CustomerName;
+        string AccType;
+        char TransactionType;
+        float amount;
+        float balance;
+
+
+        public static void Main(string[] args)
+        {
+            Program p1 = new Program(1341571, "Savings", "Savitha");
+            p1.amount = 0.1f;
+            p1.balance = 25000.00f;
+            p1.ShowData();
+            Console.WriteLine("Enter transaction type 'D' for Deposit and 'W' for Withdrawl ");
+            p1.TransactionType = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter the Amount");
+            p1.amount = Convert.ToSingle(Console.ReadLine());
+            p1.balance = p1.Ttype(p1.amount, p1.TransactionType);
+
+            //after transactions
+            Console.WriteLine("******************After Transaction*******************");
+            p1.ShowData();
+            Student s = new Student(116, "Savitha", "3year", "BCA", "6");
+            s.Getmarks();
+            s.DisplayResult();
+            s.DisplayData();
+            Console.ReadLine();
+        }
+        public Program(int num, string atype, string cname)
+        {
+            AccountNum = num;
+            AccType = atype;
+            CustomerName = cname;
+        }
+        public float Ttype(float amount, char Type)
+        {
+            if ('d' == Type)
+            {
+                return Credit(amount);
+            }
+            else if ('w' == Type)
+            {
+                return Debit(amount);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public float Credit(float deposit)
+        {
+            balance = balance + deposit;
+            return balance;
+        }
+        public float Debit(float Withdrawl)
+        {
+            balance = balance - Withdrawl;
+            return balance;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("********* Account Details**********");
+            Console.WriteLine("Account Number: " + AccountNum);
+            Console.WriteLine("Account Type: " + AccType);
+            Console.WriteLine("Balance: " + balance);
+
+        }
+    }
+    class Student
+    {
+        int rollNum;
+        string name;
+        string year;
+        string branch;
+        string sem;
+        int[] mark = new int[5];
+        int Total;
+        int avg;
+        string result;
+        int Count = 0;
+
+        public Student(int rollNum, string name, string year, string branch, string sem)
+        {
+            this.rollNum = rollNum;
+            this.name = name;
+            this.year = year;
+            this.branch = branch;
+            this.sem = sem;
+
+        }
+        public void Getmarks()
+        {
+            Console.WriteLine("Enter the marks");
+            for (int i = 0; i < mark.Length; i++)
+            {
+                Console.WriteLine("enter the marks for subject:{0} ", i + 1);
+                mark[i] = Convert.ToInt32(Console.ReadLine());
+                if (mark[i] < 35)
+                {
+                    Count++;
+                }
+
+                Total = mark[i] + Total;
+            }
+            Console.WriteLine("Total of given marks : " + Total);
+        }
+        public void DisplayResult()
+        {
+            avg = Total / mark.Length;
+
+            if (Count > 0 && avg < 50)
+            {
+                result = "Fail";
+            }
+            else
+            {
+                result = "Pass";
+            }
+
+
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("*******Student Details********");
+            Console.WriteLine("Roll Number: " + rollNum);
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Year of Studying: " + year);
+            Console.WriteLine("Branch: " + branch);
+            Console.WriteLine("Semester: " + sem);
+            Console.WriteLine("Result is: " + result);
+        }
+
+    }
+}
+
+
+

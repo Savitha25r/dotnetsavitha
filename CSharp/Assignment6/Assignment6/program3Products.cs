9d7eea0ea97c523694bc07c91d5397bcb2bc4678
savitha25r <sarithanair25r@gmail.com>
2022-10-15 20:01:26 -0700

    assignment

--- CSharp/Assignment6/Assignment6/program3Products.cs
+++ CSharp/Assignment6/Assignment6/program3Products.cs
@@ -14,7 +14,7 @@ namespace Assignment6
     class program3Products
     {
         public int ProductId { get; set; }
-        public string Product_Name { get; set; }
+        public string ProductName { get; set; }
         public float Price { get; set; }
 
 
@@ -23,16 +23,16 @@ namespace Assignment6
         {
             List<program3Products> prdlist = new List<program3Products>()
                     {
-                        new program3Products{ProductId=104,  Product_Name="Laptop",            Price=35000.0f},
-                        new program3Products{ProductId=290,  Product_Name="headset",           Price=1000.0f},
-                        new program3Products{ProductId=870,  Product_Name="Carromboardset",    Price=2499.0f},
-                        new program3Products{ProductId=250,  Product_Name="Spikeshoes",        Price=1368.0f},
-                        new program3Products{ProductId=210,  Product_Name="Hairstaightner",    Price=2799.0f},
-                        new program3Products{ProductId=400,  Product_Name="Makeupkit",         Price=999.0f},
-                        new program3Products{ProductId=280,  Product_Name="Iphone",            Price=189900.0f},
-                        new program3Products{ProductId=100,  Product_Name="RoyalEnfield",      Price=147910},
-                        new program3Products{ProductId=600,  Product_Name="Sarees",            Price=2000.0f},
-                        new program3Products{ProductId=300,  Product_Name="WashingMachine",    Price=37490.0f},
+                        new program3Products{ProductId=104,  ProductName="Laptop",            Price=35000.0f},
+                        new program3Products{ProductId=290,  ProductName="headset",           Price=1000.0f},
+                        new program3Products{ProductId=870,  ProductName="Carromboardset",    Price=2499.0f},
+                        new program3Products{ProductId=250,  ProductName="Spikeshoes",        Price=1368.0f},
+                        new program3Products{ProductId=210,  ProductName="Hairstaightner",    Price=2799.0f},
+                        new program3Products{ProductId=400,  ProductName="Makeupkit",         Price=999.0f},
+                        new program3Products{ProductId=280,  ProductName="Iphone",            Price=189900.0f},
+                        new program3Products{ProductId=100,  ProductName="RoyalEnfield",      Price=147910},
+                        new program3Products{ProductId=600,  ProductName="Sarees",            Price=2000.0f},
+                        new program3Products{ProductId=300,  ProductName="WashingMachine",    Price=37490.0f},
                    };
 
             Displayprd(prdlist);
@@ -50,7 +50,7 @@ namespace Assignment6
                 //Console.WriteLine("id: {0} ", p.ProductId);
                 //Console.WriteLine("name: {0} ", p.Product_Name);
                 //Console.WriteLine("price: {0} ", p.Price);
-                Console.WriteLine("name: {0}   \tid: {1}   \tprice: {2}    \t", p.Product_Name, p.ProductId, p.Price);
+                Console.WriteLine("name: {0}   \tid: {1}   \tprice: {2}    \t", p.ProductName, p.ProductId, p.Price);
             }
         }
     }
